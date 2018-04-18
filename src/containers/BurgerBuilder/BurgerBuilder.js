49b302fb556336c676bbe5ae0d67d7b9430b9b2c
Chiien <fuchienhsu12@gmail.com>
2018-04-17 23:50:42 -0300

    Add script to add and remove ingredient

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,6 +2,14 @@ import React, { Component, Fragment } from 'react'
 
 // import Aux from '../../hoc/Aux'
 import Burger from '../../components/Burger/Burger'
+import BurgerControls from '../../components/Burger/BurgerControls/BurgerControls'
+
+const INGREDIENTS_PRICE = {
+    salad: 0.7,
+    cheese: 0.9,
+    meat: 0.4,
+    bacon: 0.3
+}
 
 class BurgerBuilder extends Component {
 
@@ -11,14 +19,57 @@ class BurgerBuilder extends Component {
             cheese: 0,
             meat: 0,
             salad: 0
-        }
+        },
+        totalPrice: 4
+    }
+
+    addIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type]
+        const updateCount = oldCount + 1
+        const updatedIngredients = { ...this.state.ingredients }
+        updatedIngredients[type] = updateCount
+
+        const priceAddition = INGREDIENTS_PRICE[type]
+        const oldPrice = this.state.totalPrice
+        const newPrice = oldPrice + priceAddition
+        this.setState({
+            ingredients: updatedIngredients,
+            totalPrice: newPrice
+        })
+    }
+
+    removeIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type]
+        if (oldCount <= 0) return
+        const updateCount = oldCount - 1
+        const updatedIngredients = { ...this.state.ingredients }
+        updatedIngredients[type] = updateCount
+
+        const priceDeduction = INGREDIENTS_PRICE[type]
+        const oldPrice = this.state.totalPrice
+        const newPrice = oldPrice - priceDeduction
+        this.setState({
+            ingredients: updatedIngredients,
+            totalPrice: newPrice
+        })
     }
 
     render() {
+
+        const oldIngredients = { ...this.state.ingredients }
+        for (let key in oldIngredients) {
+            oldIngredients[key] = oldIngredients[key] <= 0
+        }
+
         return (
             <Fragment>
                 <Burger ingredients={this.state.ingredients} />
-                <div>Build Control</div>
+                <BurgerControls
+                    addIngredient={this.addIngredientHandler}
+                    removeIngredient={this.removeIngredientHandler}
+                    disabled={oldIngredients}
+                    totalPrice={this.state.totalPrice}
+                />
             </Fragment>
         )
     }
