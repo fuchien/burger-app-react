abdd544ac6d6cf15495cf989a5d35d0c4c2f6701
Chiien <fuchienhsu12@gmail.com>
2018-04-25 23:49:37 -0300

    Add header navigation component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,11 @@
 import React, { Fragment } from 'react'
 
-// import Aux from '../../hoc/Aux'
+import Toolbar from '../../components/Navigation/Toolbar/Toolbar'
 import classes from './Layout.css'
 
 const layout = (props) => (
     <Fragment>
-        <div>Toolbar, SideDrawer, Backdrop</div>
+        <Toolbar />
         <main className={classes.Content}>
             {props.children}
         </main>
